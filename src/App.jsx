e20ead70a967ee8f7fd48c10b9645cf51de420cd
allexib <ooomegasib@gmail.com>
2021-05-31 14:03:16 +0400

    getCash, addCash, prompt

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ function App() {
     const cash = useSelector(state => state.cash)
     console.log(cash)
 
-    const addCash = () => {
-        dispatch({type:"ADD_CASH", payload: 5})
+    const addCash = (cash) => {
+        dispatch({type:"ADD_CASH", payload: cash})
     }
 
-    const getCash = () => {
-        dispatch({type:"GET_CASH", payload: 5})
+    const getCash = (cash) => {
+        dispatch({type:"GET_CASH", payload: cash})
     }
 
   return (
     <div className={'app'}>
       <div style={{fontSize:"3rem"}}>{cash}</div>
       <div style={{display:"flex"}}>
-        <button onClick={() => addCash()}>Пополнить счет</button>
-        <button onClick={() => getCash()}>Снять со счета</button>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
       </div>
     </div>
   );
