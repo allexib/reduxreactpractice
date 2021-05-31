13a43b8a17099f4de4cad402ce8bb263f7adf180
allexib <ooomegasib@gmail.com>
2021-05-31 13:50:38 +0400

    state, reducer, action, useDispatch, useSelector start 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+
+function App() {
+  const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+    console.log(cash)
+
+  return (
+    <div className={'app'}>
+      <div style={{display:"flex"}}>
+        <button>Пополнить счет</button>
+        <button>Снять со счета</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
