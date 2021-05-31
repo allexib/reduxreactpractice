5ddd6cc9b79f41fa2b80f4313ed7341f5ea39819
allexib <ooomegasib@gmail.com>
2021-05-31 16:30:07 +0400

    create store, 2 different reducers

--- src/index.js
+++ src/index.js
@@ -3,25 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore} from "redux";
 import {Provider} from "react-redux";
+import {store} from "./store";
 
-const defaultState = {
-    cash: 5,
-}
-
-
-const reducer = (state = defaultState, action) => {
-    switch (action.type) {
-        case "ADD_CASH":
-            return {...state, cash: state.cash + action.payload}
-        case "GET_CASH":
-            return {...state, cash: state.cash - action.payload}
-        default:
-            return state
-    }
-}
-
-
-const store = createStore(reducer)
 
 ReactDOM.render(
     <Provider store={store}>
